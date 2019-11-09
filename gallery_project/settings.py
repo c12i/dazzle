963befb6eba771e2ede2ae732b528fffa57a7e96
Collins Muriuki <murerwacollins@gmail.com>
2019-11-09 21:04:15 +0300

    Reconfigure hosting settings

--- gallery_project/settings.py
+++ gallery_project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '7yk(0xuud*f0g!kr+u9m6mb_w32_$ao)oa!km(agmf@=av^(+$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.100.160"]
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ["192.168.100.160"]
 
 
 # Application definition
