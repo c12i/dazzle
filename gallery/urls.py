7f063af7cb99145c1c649f8fb93dadee0986d9fc
Collins Muriuki <murerwacollins@gmail.com>
2019-11-09 18:06:07 +0300

    Configure static url

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r"^$", views.index, name="index")
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
