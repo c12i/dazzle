bec71132e784203230ae91bd475b882c4eef47a8
Collins Muriuki <murerwacollins@gmail.com>
2019-11-09 18:07:28 +0300

    Update index view function to display all photos

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from gallery.models import Photo
 
 # Create your views here.
 def index(request):
-    return render(request, "gallery/index.html")
+    photos = Photo.show_all_photos()
+    return render(request, "gallery/index.html", context={"photos":photos})
