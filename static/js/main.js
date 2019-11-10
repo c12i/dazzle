82ce6be4448660be415037038eb807cf52e38920
Collins Muriuki <murerwacollins@gmail.com>
2019-11-10 20:53:53 +0300

    Fix image url copy bug

--- static/js/main.js
+++ static/js/main.js
@@ -18,10 +18,10 @@ function copyToClipboard(id) {
     var inputElement = document.createElement("input");
     document.body.appendChild(inputElement);
     inputElement.setAttribute("value", toCreateAttribute);
-    inputElement.setAttribute("hidden", true);
     inputElement.select();
+    inputElement.setSelectionRange(0, 99999);
     document.execCommand("copy");
     alert("Image url copied");
-
-    // document.removeChild(inputElement);
+    inputElement.setAttribute("hidden", true);
+    document.removeChild(inputElement);
   }
